3a579b9824cfe9da080bce9ed6ffa61e20ea4c67
bradham <bradham@users.noreply.github.com>
2019-08-11 22:36:24 -0500

    initial site structure and pseudo code in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+/*
+//initialize game
+set charcter points
+set attack power
+st character div positions
+
+on click move to charcter position
+move others to enemy position
+on enemy click move to defender position
+
+on 
+attack button click  write you attacked for character.attackPoints()
+update character div
+*/
